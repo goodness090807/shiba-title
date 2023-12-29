cbd9036212cdd399c3899ba2d924ef701f62bc03
Terry <b0972834939@gmail.com>
2023-12-29 09:13:55 +0800

    fix: icon路徑改為固定的url

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import "./globals.css";
 const title = "柴題遊戲";
 const description =
 	"歡迎來到柴題遊戲，這是一款結合AI圖片生成所創造的柴犬主題桌遊，這款遊戲是朋友聚會的好選擇，它提供無限的樂趣和驚喜。通過我們的隨機題目產生器，每次遊戲都會帶來不一樣的體驗，不管是桌遊愛好者或是柴犬迷，都能在這裡找到歡笑和挑戰。快來體驗，與朋友共同創造難忘的遊戲時光吧！";
+const image = `${process.env.WEB_BASE_PATH}/og-logo.png`;
 
 export const metadata = {
 	title: title,
@@ -17,7 +18,7 @@ export const metadata = {
 		type: "website",
 		images: [
 			{
-				url: `${process.env.WEB_BASE_PATH}/og-logo.png`,
+				url: image,
 				width: 800,
 				height: 800,
 			},
@@ -29,9 +30,9 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="zh-Hant-TW">
 			<head>
-				<link rel="icon" href="/favicon.svg" />
-				<link rel="shortcut icon" href="/favicon.svg" />
-				<link rel="apple-touch-icon" href="/favicon.svg" />
+				<link rel="icon" href={image} />
+				<link rel="shortcut icon" href={image} />
+				<link rel="apple-touch-icon" href={image} />
 				<meta name="robots" content="index, follow" />
 				<meta name="author" content="Terry TSAI" />
 			</head>
