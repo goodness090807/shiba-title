1d6814d9a540a34e228670dd17d71d96fc5c648e
Terry <b0972834939@gmail.com>
2024-01-08 20:39:04 +0800

    fix: script增加gtm id

--- src/app/layout.js
+++ src/app/layout.js
@@ -44,7 +44,7 @@ export default function RootLayout({ children }) {
 			</head>
 
 			{/* Google Tag Manager */}
-			<Script>
+			<Script id="google-tag-manager">
 				{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
